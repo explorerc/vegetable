15f136ef92dfa59ce2488fe4ff4680ee6d1ce3f6
quchan <chan.qu@vhall.com>
2018-09-10 16:41:31 +0800

    u

--- src/pages/watch/live.vue
+++ src/pages/watch/live.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="v-live">
-    <div class="v-video">
-      <div class="v-text">
-        6666666
-      </div>
-      <video id="myVideo" src="../../assets/image/activity3.mp4" x5-video-player-type="h5"  x5-video-player-fullscreen="false" x5-video-orientation="landscape|portrait" controls style="object-position: 0px 0px;"></video>
-      <p>
-        adfdsaffffffffffffffffffffffffffffffffasdfdasf
-      </p>
+  <div class="v-video">
+    <div class="v-text">
+      6666666
     </div>
+    <video id="myVideo" src="../../assets/image/activity3.mp4" x5-video-player-type="h5"  x5-video-player-fullscreen="false" x5-video-orientation="landscape|portrait" controls style="object-position: 0px 0px;"></video>
+    <p>
+      adfdsaffffffffffffffffffffffffffffffffasdfdasf
+    </p>
   </div>
 </template>
 <script>
@@ -37,25 +35,21 @@
   }
 </script>
 <style lang="scss" scoped>
-.v-live {
+.v-video {
   width: 100%;
   height: 100%;
-  .v-video {
+  position: relative;
+  .v-text {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    z-index: 2;
+  }
+  video {
     width: 100%;
-    height: 100%;
-    position: relative;
-    .v-text {
-      position: absolute;
-      top: 20px;
-      left: 20px;
-      z-index: 2;
-    }
-    video {
-      width: 100%;
-    }
-    p {
-      color: #fff;
-    }
+  }
+  p {
+    color: #fff;
   }
 }
 </style>
