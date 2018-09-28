616d60393afcddf52d87790506897ed661898f77
xiaoguang.hu <xiaoguang.hu@vhall.com>
2018-09-28 19:05:19 +0800

    m

--- build/config.js
+++ build/config.js
@@ -5,7 +5,7 @@ const config = {
     API_PATH: '/api',
     SUB_DIR: '',
     PUBLIC_PATH: '/',
-    IMGHOST: 'http://dev-zhike.oss-cn-beijing.aliyuncs.com',
+    IMGHOST: '//dev-zhike.oss-cn-beijing.aliyuncs.com',
     proxyTable: {
       '/api': {
         target: 'http://dev-api-zhike.vhall.domain',
@@ -21,23 +21,23 @@ const config = {
     API_PATH: '/api',
     SUB_DIR: 'static',
     PUBLIC_PATH: '//dev-zhike.vhall.domain/',
-    IMGHOST: 'http://dev-zhike.oss-cn-beijing.aliyuncs.com',
-    //PUBLIC_PATH: '//127.0.0.1:8088/'
+    IMGHOST: '//dev-zhike.oss-cn-beijing.aliyuncs.com'
+    // PUBLIC_PATH: '//127.0.0.1:8088/'
   },
   test: {
     BUILD_ENV: 'test',
-    API_PATH: '//t-api-class.e.vhall.com',
+    API_PATH: '/watch/api',
     SUB_DIR: 'static',
-    PUBLIC_PATH: '//t-static01-class.e.vhall.com/vhallclass/',
-    IMGHOST: 'http://dev-zhike.oss-cn-beijing.aliyuncs.com',
+    PUBLIC_PATH: '//test-zhike.vhall.com/m/',
+    IMGHOST: '//test-zhike.oss-cn-beijing.aliyuncs.com'
   },
   prod: {
     BUILD_ENV: 'prod',
     API_PATH: '//api-class.e.vhall.com',
     SUB_DIR: 'static',
     PUBLIC_PATH: '//static01-class.e.vhall.com/vhallclass/',
-    IMGHOST: 'http://dev-zhike.oss-cn-beijing.aliyuncs.com',
+    IMGHOST: 'http://dev-zhike.oss-cn-beijing.aliyuncs.com'
   }
 }
 
-module.exports = config;
+module.exports = config
