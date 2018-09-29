8f06f6726ab30d262738f83d6da673eb2db3a6a4
xiaoguang.hu <xiaoguang.hu@vhall.com>
2018-09-29 18:46:29 +0800

    m

--- src/assets/js/wx-share.js
+++ src/assets/js/wx-share.js
@@ -16,84 +16,84 @@ export default function wxShare (wxShare) {
       'onMenuShareQZone'
     ]
   })
-  wx.ready(function () {
-    wx.checkJsApi({
-      jsApiList: [
-        'onMenuShareTimeline',
-        'onMenuShareAppMessage',
-        'onMenuShareQQ',
-        'onMenuShareWeibo',
-        'onMenuShareQZone'
-      ], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-      success: function (res) {
-        // 以键值对的形式返回，可用的api值true，不可用为false
-        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-        console.log(res.checkResult, 789486)
-      }
-    })
-    wx.onMenuShareTimeline({
-      title: wxShare.shareData.title, // 分享标题
-      desc: wxShare.shareDatadesc,
-      link: wxShare.shareDatalink + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-3' : ''), // 分享链接
-      imgUrl: wxShare.shareDataimgUrl, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    })
-    wx.onMenuShareAppMessage({
-      title: wxShare.shareData.title, // 分享标题
-      desc: wxShare.shareData.desc,
-      link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-3' : ''), // 分享链接
-      imgUrl: wxShare.shareData.imgUrl, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    })
-    wx.onMenuShareQQ({
-      title: wxShare.shareData.title, // 分享标题
-      desc: wxShare.shareData.desc,
-      link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-2' : ''), // 分享链接
-      imgUrl: wxShare.shareData.imgUrl, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    })
-    wx.onMenuShareWeibo({
-      title: wxShare.shareData.title, // 分享标题
-      desc: wxShare.shareData.desc,
-      link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-1' : ''), // 分享链接
-      imgUrl: wxShare.shareData.imgUrl, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    })
-    wx.onMenuShareQZone({
-      title: wxShare.shareData.title, // 分享标题
-      desc: wxShare.shareData.desc,
-      link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-2' : ''), // 分享链接
-      imgUrl: wxShare.shareData.imgUrl, // 分享图标
-      success: function () {
-        // 用户确认分享后执行的回调函数
-      },
-      cancel: function () {
-        // 用户取消分享后执行的回调函数
-      }
-    })
-  })
-  wx.error(function (res) {
-    // $("#chatlist").append("<p>"+JSON.stringify(res)+"</p>");
-    console.log(res, 6666)
-  })
+  // wx.ready(function () {
+  //   wx.checkJsApi({
+  //     jsApiList: [
+  //       'onMenuShareTimeline',
+  //       'onMenuShareAppMessage',
+  //       'onMenuShareQQ',
+  //       'onMenuShareWeibo',
+  //       'onMenuShareQZone'
+  //     ], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+  //     success: function (res) {
+  //       // 以键值对的形式返回，可用的api值true，不可用为false
+  //       // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+  //       console.log(res.checkResult, 789486)
+  //     }
+  //   })
+  //   wx.onMenuShareTimeline({
+  //     title: wxShare.shareData.title, // 分享标题
+  //     desc: wxShare.shareDatadesc,
+  //     link: wxShare.shareDatalink + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-3' : ''), // 分享链接
+  //     imgUrl: wxShare.shareDataimgUrl, // 分享图标
+  //     success: function () {
+  //       // 用户确认分享后执行的回调函数
+  //     },
+  //     cancel: function () {
+  //       // 用户取消分享后执行的回调函数
+  //     }
+  //   })
+  //   wx.onMenuShareAppMessage({
+  //     title: wxShare.shareData.title, // 分享标题
+  //     desc: wxShare.shareData.desc,
+  //     link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-3' : ''), // 分享链接
+  //     imgUrl: wxShare.shareData.imgUrl, // 分享图标
+  //     success: function () {
+  //       // 用户确认分享后执行的回调函数
+  //     },
+  //     cancel: function () {
+  //       // 用户取消分享后执行的回调函数
+  //     }
+  //   })
+  //   wx.onMenuShareQQ({
+  //     title: wxShare.shareData.title, // 分享标题
+  //     desc: wxShare.shareData.desc,
+  //     link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-2' : ''), // 分享链接
+  //     imgUrl: wxShare.shareData.imgUrl, // 分享图标
+  //     success: function () {
+  //       // 用户确认分享后执行的回调函数
+  //     },
+  //     cancel: function () {
+  //       // 用户取消分享后执行的回调函数
+  //     }
+  //   })
+  //   wx.onMenuShareWeibo({
+  //     title: wxShare.shareData.title, // 分享标题
+  //     desc: wxShare.shareData.desc,
+  //     link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-1' : ''), // 分享链接
+  //     imgUrl: wxShare.shareData.imgUrl, // 分享图标
+  //     success: function () {
+  //       // 用户确认分享后执行的回调函数
+  //     },
+  //     cancel: function () {
+  //       // 用户取消分享后执行的回调函数
+  //     }
+  //   })
+  //   wx.onMenuShareQZone({
+  //     title: wxShare.shareData.title, // 分享标题
+  //     desc: wxShare.shareData.desc,
+  //     link: wxShare.shareData.link + (wxShare.shareUser.share_id ? '?shareId=' + wxShare.shareUser.share_id + '-2' : ''), // 分享链接
+  //     imgUrl: wxShare.shareData.imgUrl, // 分享图标
+  //     success: function () {
+  //       // 用户确认分享后执行的回调函数
+  //     },
+  //     cancel: function () {
+  //       // 用户取消分享后执行的回调函数
+  //     }
+  //   })
+  // })
+  // wx.error(function (res) {
+  //   // $("#chatlist").append("<p>"+JSON.stringify(res)+"</p>");
+  //   console.log(res, 6666)
+  // })
 }
