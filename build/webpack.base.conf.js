d0f0ceba1eda9d9db57d9710e44739ad0953b70c
xiaoguang.hu <xiaoguang.hu@vhall.com>
2018-09-29 18:29:15 +0800

    m

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
@@ -8,16 +8,16 @@ const happyThreadPool = HappyPack.ThreadPool({
   size: os.cpus().length
 })
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const config = require('./config');
+const config = require('./config')
 
 const isProd = process.env.NODE_ENV === 'production'
 
-function resolve(dir) {
+function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-function subPath(_path) {
-  return path.posix.join(config[process.env.BUILD_ENV].SUB_DIR, _path);
+function subPath (_path) {
+  return path.posix.join(config[process.env.BUILD_ENV].SUB_DIR, _path)
 }
 
 const webpackConfig = {
@@ -33,24 +33,22 @@ const webpackConfig = {
     publicPath: config[process.env.BUILD_ENV].PUBLIC_PATH
   },
   externals: {
-    'vue': 'Vue',
-    'element-ui': 'ELEMENT',
+    vue: 'Vue',
+    'element-ui': 'ELEMENT'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
-    modules: [
-      resolve('src'),
-      resolve('node_modules')
-    ],
+    modules: [resolve('src'), resolve('node_modules')],
     alias: {
-      'src': resolve('src'),
-      'assets': resolve('src/assets'),
-      'components': resolve('src/components'),
-      'store': resolve('src/store')
+      src: resolve('src'),
+      assets: resolve('src/assets'),
+      components: resolve('src/components'),
+      store: resolve('src/store')
     }
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.(vue|js|jsx)$/,
         loader: 'eslint-loader',
         include: resolve('src'),
@@ -69,35 +67,41 @@ const webpackConfig = {
       },
       {
         test: /.(png|jpg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            name: subPath('img/[name].[ext]'),
-            limit: 500 //单位是byte
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: subPath('img/[name].[ext]'),
+              limit: 500 // 单位是byte
+            }
           }
-        }],
+        ],
         include: resolve('src')
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 5000,
-            name: subPath('media/[name].[ext]')
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              name: subPath('media/[name].[ext]')
+            }
           }
-        }],
+        ],
         include: resolve('src')
       },
       {
         test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 50,
-            name: subPath('fonts/[name].[ext]')
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 50,
+              name: subPath('fonts/[name].[ext]')
+            }
           }
-        }],
+        ],
         include: resolve('src')
       }
     ]
@@ -105,12 +109,12 @@ const webpackConfig = {
   optimization: {
     splitChunks: {
       cacheGroups: {
-        common: {
-          name: 'common',
-          chunks: 'async',
-          minChunks: 2,
-          minSize: 0
-        },
+        // common: {
+        //   name: 'common',
+        //   chunks: 'async',
+        //   minChunks: 2,
+        //   minSize: 0
+        // },
         vender: {
           name: 'vendor',
           test: resolve('node_modules'),
@@ -143,12 +147,14 @@ const webpackConfig = {
         PUBLIC_PATH: JSON.stringify(config[process.env.BUILD_ENV].PUBLIC_PATH)
       }
     }),
-    new CopyWebpackPlugin([{
-      from: resolve('src/static'),
-      to: resolve(`dist/${config[process.env.BUILD_ENV].SUB_DIR}`),
-      ignore: ['.*']
-    }])
+    new CopyWebpackPlugin([
+      {
+        from: resolve('src/static'),
+        to: resolve(`dist/${config[process.env.BUILD_ENV].SUB_DIR}`),
+        ignore: ['.*']
+      }
+    ])
   ]
-};
+}
 
-module.exports = webpackConfig;
+module.exports = webpackConfig
