a93c8ca4ef5f18297a72d60f84eb4794d72f3945
jinpeng <peng.jin@vhall.com>
2018-10-17 11:02:03 +0800

    add page

--- src/pages/test/fail.vue
+++ src/pages/test/fail.vue
@@ -1,5 +1,10 @@
 <template>
-<div class='wrap'>success</div>
+<div class='wrap'>
+  <dl>
+    <dt></dt>
+    <dd>发送失败！</dd>
+  </dl>
+</div>
 </template>
 
 <script>
@@ -10,5 +15,27 @@ export default {
 
 <style scoped lang="scss">
 .wrap {
+  width: 100%;
+  height: 100%;
+  dl {
+    width: 100%;
+    height: 400px;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    margin-top: -200px;
+    dt {
+      width: 100%;
+      height: 300px;
+      background: url('~assets/image/test-fail.jpg') no-repeat center;
+      background-size: contain;
+    }
+    dd {
+      text-align: center;
+      font-size: 50px;
+      padding-left: 40px;
+      padding-top: 20px;
+    }
+  }
 }
 </style>
--- src/pages/test/success.vue
+++ src/pages/test/success.vue
@@ -1,5 +1,10 @@
 <template>
-<div class='wrap'>success</div>
+<div class='wrap'>
+  <dl>
+    <dt></dt>
+    <dd>发送成功！</dd>
+  </dl>
+</div>
 </template>
 
 <script>
@@ -9,4 +14,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.wrap {
+  width: 100%;
+  height: 100%;
+  dl {
+    width: 100%;
+    height: 400px;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    margin-top: -200px;
+    dt {
+      width: 100%;
+      height: 300px;
+      background: url('~assets/image/test-success.jpg') no-repeat center;
+      background-size: contain;
+    }
+    dd {
+      text-align: center;
+      font-size: 50px;
+      padding-left: 40px;
+      padding-top: 20px;
+    }
+  }
+}
 </style>
