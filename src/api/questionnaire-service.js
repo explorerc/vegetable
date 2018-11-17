a2c7ffc7b0e7244a77b6c68008d4263e27ce33bb
quchan <chan.qu@vhall.com>
2018-11-17 17:50:45 +0800

    u

--- src/api/questionnaire-service.js
+++ src/api/questionnaire-service.js
@@ -1,4 +1,5 @@
 export default {
   // 获取地区数据
-  GET_AREA_JSON: 'https://static.vhallyun.com/public/static/js/area.json'
+  GET_AREA_JSON: 'https://static.vhallyun.com/public/static/js/area.json',
+  GET_QUESTION: '/frontend/tool/get-naire'
 }
