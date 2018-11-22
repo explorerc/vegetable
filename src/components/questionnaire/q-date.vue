f1f0491455c37095a5fcc02eeebd7958dabbc082
quchan <chan.qu@vhall.com>
2018-11-22 15:32:56 +0800

    u

--- src/components/questionnaire/q-date.vue
+++ src/components/questionnaire/q-date.vue
@@ -5,7 +5,8 @@
                     type="date"
                     @focus="focus"
                     value-format="yyyy-MM-dd"
-                    placeholder="选择日期">
+                    placeholder="选择日期"
+                    :picker-options="pickerOptions">
     </el-date-picker>
     <div v-if="!edit&&errorTip"
          class="error-msg">{{errorTip}}
@@ -30,7 +31,12 @@ export default {
   data () {
     return {
       val: '',
-      errorTip: ''
+      errorTip: '',
+      pickerOptions: {
+        disabledDate (time) {
+          return time.getTime() > Date.now()
+        }
+      }
     }
   },
   methods: {
