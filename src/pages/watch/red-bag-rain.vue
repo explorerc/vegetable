a13f69de8836fa256cdbb263c479cd945168a101
your_name <your_email>
2018-11-24 15:36:12 +0800

    s

--- src/pages/watch/red-bag-rain.vue
+++ src/pages/watch/red-bag-rain.vue
@@ -189,7 +189,7 @@
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, .5);
-    z-index: 9999;
+    z-index: 1000;
     .rain-num {
       display: inline-block;
       position: absolute;
