ef173651d12a9b69223d238e6d604db934517a77
your_name <your_email>
2018-12-28 11:13:03 +0800

    优化登录

--- src/components/common/dialog/com.vue
+++ src/components/common/dialog/com.vue
@@ -20,106 +20,108 @@
 </template>
 
 <script>
-export default {
-  name: 'ComDialog',
-  props: {
-    header: String,
-    content: String,
-    center: Boolean,
-    visible: Boolean,
-    beforeClose: Function,
-    dialogClose: Boolean
-  },
-  data () {
-    return {
-      innerVisible: false
-    }
-  },
-  methods: {
-    wrapClose () {
-      if (this.dialogClose) {
-        this.handleClose()
+  export default {
+    name: 'ComDialog',
+    props: {
+      header: String,
+      content: String,
+      center: Boolean,
+      visible: Boolean,
+      beforeClose: Function,
+      dialogClose: Boolean
+    },
+    data () {
+      return {
+        innerVisible: false
       }
     },
-    handleClose () {
-      if (typeof this.beforeClose === 'function') {
-        this.beforeClose(this.hide)
-      } else {
-        this.hide()
-      }
-    },
-    hide () {
-      this.innerVisible = false
-      this.$emit('update:visible', false)
-      this.$emit('close')
-    }
-  },
-  watch: {
-    visible: {
-      handler (value) {
-        if (value) {
-          setTimeout(() => {
-            this.innerVisible = value
-          }, 50)
+    methods: {
+      wrapClose () {
+        if (this.dialogClose) {
+          this.handleClose()
         }
       },
-      immediate: true
+      handleClose () {
+        if (typeof this.beforeClose === 'function') {
+          this.beforeClose(this.hide)
+        } else {
+          this.hide()
+        }
+      },
+      hide () {
+        this.innerVisible = false
+        this.$emit('update:visible', false)
+        this.$emit('close')
+      }
+    },
+    watch: {
+      visible: {
+        handler (value) {
+          if (value) {
+            setTimeout(() => {
+              this.innerVisible = value
+            }, 50)
+          }
+        },
+        immediate: true
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.com-dialog /deep/ {
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.3s;
-  }
-
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-  }
-  position: fixed;
-  z-index: 2000;
-  background-color: rgba(0, 0, 0, 0.5);
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  .dialog-wrap {
-    width: 30%;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background: #fff;
-    border-radius: 2px;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
-    box-sizing: border-box;
-    color: #606266;
-    font-size: 14px;
-    .align-center {
-      text-align: center;
+  .com-dialog /deep/ {
+    .fade-enter-active,
+    .fade-leave-active {
+      transition: opacity 0.3s;
     }
-    .dialog-header {
-      padding: 20px;
-      padding-bottom: 10px;
-      font-size: 18px;
-      color: #303133;
-      .icon-close {
-        position: absolute;
-        cursor: pointer;
-        top: 20px;
-        right: 20px;
-        &:hover {
-          color: #409eff;
+
+    .fade-enter,
+    .fade-leave-to {
+      opacity: 0;
+    }
+    position: fixed;
+    z-index: 2000;
+    background-color: rgba(0, 0, 0, 0.5);
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    .dialog-wrap {
+      width: 30%;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: #fff;
+      border-radius: 2px;
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+      box-sizing: border-box;
+      color: #606266;
+      font-size: 14px;
+      .align-center {
+        text-align: center;
+      }
+      .dialog-header {
+        padding: 20px;
+        padding-bottom: 10px;
+        font-size: 18px;
+        color: #303133;
+        .icon-close {
+          position: absolute;
+          cursor: pointer;
+          top: 20px;
+          right: 20px;
+          font-size: 40px;
+          color: #666;
+          &:hover {
+            color: #888;
+          }
         }
       }
-    }
-    .dialog-body {
-      padding: 30px 20px;
+      .dialog-body {
+        padding: 30px 20px;
+      }
     }
   }
-}
 </style>
--- src/components/common/input/com.vue
+++ src/components/common/input/com.vue
@@ -49,305 +49,305 @@
 </template>
 
 <script>
-export default {
-  name: 'ComInput',
-  props: {
-    placeholder: String,
-    value: String,
-    type: {
-      type: String,
-      default: 'input'
-    },
-    max: [Number, String],
-    maxLength: Number,
-    rows: {
-      type: Number,
-      default: 2
-    },
-    autosize: Boolean,
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    errorTips: String,
-    isCharacter: {
-      type: Boolean,
-      default: false
-    },
-    isDelete: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data () {
-    return {
-      num: '',
-      isMobile: false,
-      innerValue: '',
-      showDelete: false,
-      inputType: '',
-      offsetHeight: 0,
-      errorMsg: '',
-      limitColor: '#555' // 文字字数颜色，0为#555,大于0为#4b5afe
-    }
-  },
-  created () {
-    this.inputType = this.getType()
-  },
-  mounted () {
-    if (this.$refs.tarea) {
-      this.offsetHeight = this.$refs.tarea.offsetHeight - this.$refs.tarea.clientHeight
-    }
-    this.innerValue = this.value
-    this.inputType = this.getType()
-  },
-  methods: {
-    focusHandle (e) {
-      this.showDelete = true
-      this.$emit('focus', e)
-    },
-    blurHandle (e) {
-      this.hideDelete()
-      this.$emit('blur', e)
-    },
-    empty () {
-      this.innerValue = ''
-    },
-    toggleShow () {
-      if (this.inputType === 'password') {
-        this.inputType = 'text'
-      } else {
-        this.inputType = 'password'
+  export default {
+    name: 'ComInput',
+    props: {
+      placeholder: String,
+      value: String,
+      type: {
+        type: String,
+        default: 'input'
+      },
+      max: [Number, String],
+      maxLength: Number,
+      rows: {
+        type: Number,
+        default: 2
+      },
+      autosize: Boolean,
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      errorTips: String,
+      isCharacter: {
+        type: Boolean,
+        default: false
+      },
+      isDelete: {
+        type: Boolean,
+        default: false
       }
     },
-    hideDelete () {
-      setTimeout(() => {
-        this.showDelete = false
-      }, 200)
-    },
-    getType () {
-      let type = ''
-      switch (this.type) {
-        case 'password':
-          type = 'password'
-          break
-        case 'mobile':
-          this.isMobile = true
-          type = 'text'
-          break
-
-        default:
-          type = 'text'
-          break
+    data () {
+      return {
+        num: '',
+        isMobile: false,
+        innerValue: '',
+        showDelete: false,
+        inputType: '',
+        offsetHeight: 0,
+        errorMsg: '',
+        limitColor: '#555' // 文字字数颜色，0为#555,大于0为#4b5afe
       }
-      return type
-    }
-  },
-  watch: {
-    innerValue (value) {
-      if (value === undefined) {
+    },
+    created () {
+      this.inputType = this.getType()
+    },
+    mounted () {
+      if (this.$refs.tarea) {
+        this.offsetHeight = this.$refs.tarea.offsetHeight - this.$refs.tarea.clientHeight
+      }
+      this.innerValue = this.value
+      this.inputType = this.getType()
+    },
+    methods: {
+      focusHandle (e) {
+        this.showDelete = true
+        this.$emit('focus', e)
+      },
+      blurHandle (e) {
+        this.hideDelete()
+        this.$emit('blur', e)
+      },
+      empty () {
         this.innerValue = ''
+      },
+      toggleShow () {
+        if (this.inputType === 'password') {
+          this.inputType = 'text'
+        } else {
+          this.inputType = 'password'
+        }
+      },
+      hideDelete () {
+        setTimeout(() => {
+          this.showDelete = false
+        }, 200)
+      },
+      getType () {
+        let type = ''
+        switch (this.type) {
+          case 'password':
+            type = 'password'
+            break
+          case 'mobile':
+            this.isMobile = true
+            type = 'text'
+            break
+
+          default:
+            type = 'text'
+            break
+        }
+        return type
+      }
+    },
+    watch: {
+      innerValue (value) {
+        if (value === undefined) {
+          this.innerValue = ''
+          this.$emit('update:value', this.innerValue)
+          this.$emit('input', this.innerValue)
+          return
+        }
+        if (this.type === 'float') {
+          let match = value.match(/\d{1,8}(\.\d{0,2})?/)
+          let _value = match ? match[0] : ''
+          if (this.max && _value - 0 > this.max - 0) {
+            _value = this.max
+          }
+          this.innerValue = _value
+        } else if (this.isMobile) {
+          this.innerValue = value.replace(/\D/g, '')
+          if (this.maxLength && value.length > this.maxLength) {
+            this.innerValue = value.substring(0, this.maxLength)
+          }
+        } else if (this.type === 'number') {
+          this.innerValue = value.replace(/\D/g, '')
+          if (this.maxLength && value.length > this.maxLength) {
+            this.innerValue = value.substring(0, this.maxLength)
+          }
+        } else if (this.isCharacter) {
+          if (this.maxLength && value.length > this.maxLength) {
+            this.innerValue = value.substring(0, this.maxLength)
+          }
+        } else if (this.maxLength && value.gbLength() > this.maxLength) {
+          this.innerValue = value.substring(0, value.gbIndex(this.maxLength) + 1)
+        }
+        if (this.type === 'textarea' && this.autosize) {
+          this.$refs.tarea.style.height = 'auto'
+          this.$refs.tarea.style.height = `${this.$refs.tarea.scrollHeight + this.offsetHeight}px`
+        }
+        if (value.gbLength() === 0) {
+          this.limitColor = '#555'
+        } else {
+          this.limitColor = '#4b5afe'
+        }
         this.$emit('update:value', this.innerValue)
         this.$emit('input', this.innerValue)
-        return
-      }
-      if (this.type === 'float') {
-        let match = value.match(/\d{1,8}(\.\d{0,2})?/)
-        let _value = match ? match[0] : ''
-        if (this.max && _value - 0 > this.max - 0) {
-          _value = this.max
-        }
-        this.innerValue = _value
-      } else if (this.isMobile) {
-        this.innerValue = value.replace(/\D/g, '')
-        if (this.maxLength && value.length > this.maxLength) {
-          this.innerValue = value.substring(0, this.maxLength)
-        }
-      } else if (this.type === 'number') {
-        this.innerValue = value.replace(/\D/g, '')
-        if (this.maxLength && value.length > this.maxLength) {
-          this.innerValue = value.substring(0, this.maxLength)
-        }
-      } else if (this.isCharacter) {
-        if (this.maxLength && value.length > this.maxLength) {
-          this.innerValue = value.substring(0, this.maxLength)
-        }
-      } else if (this.maxLength && value.gbLength() > this.maxLength) {
-        this.innerValue = value.substring(0, value.gbIndex(this.maxLength) + 1)
-      }
-      if (this.type === 'textarea' && this.autosize) {
-        this.$refs.tarea.style.height = 'auto'
-        this.$refs.tarea.style.height = `${this.$refs.tarea.scrollHeight + this.offsetHeight}px`
-      }
-      if (value.gbLength() === 0) {
-        this.limitColor = '#555'
-      } else {
-        this.limitColor = '#4b5afe'
-      }
-      this.$emit('update:value', this.innerValue)
-      this.$emit('input', this.innerValue)
-    },
-    value: {
-      handler (value) {
-        this.innerValue = value
-        this.$emit('change')
       },
-      immediate: true
-    },
-    type: {
-      handler (value) {
-        this.inputType = this.getType()
+      value: {
+        handler (value) {
+          this.innerValue = value
+          this.$emit('change')
+        },
+        immediate: true
       },
-      immediate: true
-    },
-    errorTips: {
-      handler (value) {
-        this.errorMsg = value
+      type: {
+        handler (value) {
+          this.inputType = this.getType()
+        },
+        immediate: true
       },
-      immediate: true
-    }
-  },
-  computed: {
-    style () {
-      const ret = {}
+      errorTips: {
+        handler (value) {
+          this.errorMsg = value
+        },
+        immediate: true
+      }
+    },
+    computed: {
+      style () {
+        const ret = {}
 
-      if (this.type === 'search') {
-        ret.paddingLeft = '36px'
-        ret.paddingRight = '30px'
-      } else if (this.maxLength) {
-        ret.paddingRight = '45px'
-      } else if (this.type === 'password') {
-        ret.paddingRight = '30px'
+        if (this.type === 'search') {
+          ret.paddingLeft = '36px'
+          ret.paddingRight = '30px'
+        } else if (this.maxLength) {
+          ret.paddingRight = '45px'
+        } else if (this.type === 'password') {
+          ret.paddingRight = '30px'
+        }
+        return ret
       }
-      return ret
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.com-input /deep/ {
-  position: relative;
-  display: inline-block;
-  width: 100%;
-  height: 90px;
-  .error-msg {
-    display: block;
-    position: absolute;
-    color: #fc5659;
-    padding-left: 10px;
-    font-size: 14px;
-  }
-  input {
+  .com-input /deep/ {
+    position: relative;
     display: inline-block;
     width: 100%;
     height: 90px;
-    font-size: 28px;
-    line-height: 1.2 !important;
-    color: rgba(0, 0, 0, 0.65);
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-    transition: all 0.3s;
-    padding: 0 10px;
-    &.error {
-      border-color: #fc5659;
+    .error-msg {
+      display: block;
+      position: absolute;
+      color: #fc5659;
+      padding-left: 10px;
+      font-size: 14px;
     }
-    &:hover {
-      border-color: #4b5afe;
+    input {
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      font-size: 28px;
+      line-height: 1.2 !important;
+      color: rgba(0, 0, 0, 0.65);
+      background-color: #fff;
+      background-image: none;
+      border: 1px solid #d9d9d9;
+      border-radius: 4px;
+      transition: all 0.3s;
+      padding: 0 10px;
+      &.error {
+        border-color: #fc5659;
+      }
+      &:hover {
+        border-color: #4b5afe;
+      }
+      /*&:focus {*/
+      /*box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);*/
+      /*}*/
+      &::-moz-placeholder {
+        color: #bfbfbf;
+        opacity: 1;
+      }
+      &:-ms-input-placeholder {
+        color: #bfbfbf;
+      }
+      &::-webkit-input-placeholder {
+        color: #bfbfbf;
+      }
+      &[disabled] {
+        cursor: not-allowed;
+      }
     }
-    /*&:focus {*/
-    /*box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);*/
-    /*}*/
-    &::-moz-placeholder {
-      color: #bfbfbf;
-      opacity: 1;
+    .limit {
+      font-size: 14px;
+      color: #555555;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 8px;
+      .length {
+        color: #4b5afe;
+      }
+      &.area {
+        transform: none;
+        top: auto;
+        bottom: 10px;
+      }
     }
-    &:-ms-input-placeholder {
-      color: #bfbfbf;
+    .icon-search {
+      color: #555555;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 12px;
     }
-    &::-webkit-input-placeholder {
-      color: #bfbfbf;
+    .icon-right-center {
+      color: #555555;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 8px;
+      cursor: pointer;
     }
-    &[disabled] {
-      cursor: not-allowed;
+    .icon-delete {
+      @extend .icon-right-center;
     }
-  }
-  .limit {
-    font-size: 14px;
-    color: #555555;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    right: 8px;
-    .length {
-      color: #4b5afe;
+    .icon-guanbi-yanjing {
+      @extend .icon-right-center;
     }
-    &.area {
-      transform: none;
-      top: auto;
-      bottom: 10px;
+    .icon-faxian-yanjing {
+      @extend .icon-right-center;
     }
-  }
-  .icon-search {
-    color: #555555;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    left: 12px;
-  }
-  .icon-right-center {
-    color: #555555;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    right: 8px;
-    cursor: pointer;
-  }
-  .icon-delete {
-    @extend .icon-right-center;
-  }
-  .icon-guanbi-yanjing {
-    @extend .icon-right-center;
-  }
-  .icon-faxian-yanjing {
-    @extend .icon-right-center;
-  }
 
-  textarea {
-    display: inline-block;
-    padding: 4px 11px;
-    width: 100%;
-    height: 100%;
-    font-size: 14px;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, 0.65);
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-    transition: all 0.3s;
-    font-size: inherit;
-    &.error {
-      border-color: #fc5659;
-    }
-    &:hover {
-      border-color: #4b5afe;
-    }
-    &::-moz-placeholder {
-      color: #bfbfbf;
-      opacity: 1;
-    }
-    &:-ms-input-placeholder {
-      color: #bfbfbf;
-    }
-    &::-webkit-input-placeholder {
-      color: #bfbfbf;
-    }
-    &[disabled] {
-      cursor: not-allowed;
+    textarea {
+      display: inline-block;
+      padding: 4px 11px;
+      width: 100%;
+      height: 100%;
+      font-size: 14px;
+      line-height: 1.5;
+      color: rgba(0, 0, 0, 0.65);
+      background-color: #fff;
+      background-image: none;
+      border: 1px solid #d9d9d9;
+      border-radius: 4px;
+      transition: all 0.3s;
+      font-size: inherit;
+      &.error {
+        border-color: #fc5659;
+      }
+      &:hover {
+        border-color: #4b5afe;
+      }
+      &::-moz-placeholder {
+        color: #bfbfbf;
+        opacity: 1;
+      }
+      &:-ms-input-placeholder {
+        color: #bfbfbf;
+      }
+      &::-webkit-input-placeholder {
+        color: #bfbfbf;
+      }
+      &[disabled] {
+        cursor: not-allowed;
+      }
     }
   }
-}
 </style>
