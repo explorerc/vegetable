3919a334cd8b2ba86e0beaf06f37b6ca47c03764
explorerc <459374225@qq.com>
2019-05-15 18:44:36 +0800

    'route'

--- src/api/user-service.js
+++ src/api/user-service.js
@@ -1,6 +1,6 @@
 export default {
-  GET_USER_INFO: 'http://vegetable.utools.club/user/info', // 获取用户信息
-  GET_USER_ID: 'http://vegetable.utools.club/index/getBaseInfo',
-  GET_USER_ADD: 'http://vegetable.utools.club/user/add',
+  GET_USER_INFO: 'http://mg.vegetable.umcoder.com/public/user/info', // 获取用户信息
+  GET_USER_ID: 'http://mg.vegetable.umcoder.com/public/index/getBaseInfo',
+  GET_USER_ADD: 'http://mg.vegetable.umcoder.com/public/user/add',
   GET_USER_UPDATE: 'http://mg.vegetable.umcoder.com/public/user/update' // 根据用户id修改信息
 }
